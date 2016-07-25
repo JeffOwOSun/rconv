9c9ee768f99c90cab8f769b4a20e0949cfa02fca
Hengyuan Hu <kevin.huhy@gmail.com>
2016-07-24 21:53:00 -0700

    fix type in 'test_conv/load_net.py'.

--- test_rconv/load_net.py
+++ test_rconv/load_net.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
 
     lenet_ptx = os.path.join(os.path.dirname(__file__), 'lenet.prototxt')
     lenet_ptx = str(lenet_ptx) # what the hack is happening here?
-    model = os.path,join(os.path.dirname(__file__), 'init.caffemodel')
+    model = os.path.join(os.path.dirname(__file__), 'init.caffemodel')
+    model = str(model)
     # net = caffe.Net(lenet_ptx, caffe.TRAIN)
-    net = caffe.Net(lenet_ptx, 'init.caffemodel', caffe.TRAIN)
+    net = caffe.Net(lenet_ptx, model, caffe.TRAIN)
 
