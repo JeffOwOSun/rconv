37e4521d04273ad0f0702598f7946f1cca2556bf
Hengyuan Hu <kevin.huhy@gmail.com>
2016-07-24 20:50:42 -0700

    update test environment: use caffe.TRAIN mode for bp.

--- test_rconv/load_net.py
+++ test_rconv/load_net.py
@@ -7,5 +7,7 @@ if __name__ == '__main__':
 
     lenet_ptx = os.path.join(os.path.dirname(__file__), 'lenet.prototxt')
     lenet_ptx = str(lenet_ptx) # what the hack is happening here?
-    net = caffe.Net(lenet_ptx,caffe.TEST)
+    model = os.path,join(os.path.dirname(__file__), 'init.caffemodel')
+    # net = caffe.Net(lenet_ptx, caffe.TRAIN)
+    net = caffe.Net(lenet_ptx, 'init.caffemodel', caffe.TRAIN)
 
